81e90d0ddb6511254ccd66dc1c311bb17aa48a29
Mark Siazon <markangelosiazon@gmail.com>
2024-12-20 00:20:30 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/FM-Huddle-Landing-Page/",
+  server: {
+    host: true,
+  },
+  css: {
+    postcss: 'postcss.config.cjs',
+  },
+});
