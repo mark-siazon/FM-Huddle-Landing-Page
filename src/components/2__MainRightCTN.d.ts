ee7f177ea0b2dfc4092749666c42c95d7a001534
Mark Siazon <markangelosiazon@gmail.com>
2025-01-06 22:45:06 +0800

    ✈️ | Deployment + Add TypeScript declaration files for deployment
    (Auto-generated by npm run build)
      - 2_MainRightCTN.d.ts
      - 2.1_LandingPriBtn.d.ts
      - 2.1.1_Modal.d.ts
      - 2.2_SocmedSectBtn.d.ts
    - Included App.d.ts and main.d.ts for app-level typings.
    - Updated tsconfig.json and generated tsconfig.tsbuildinfo.

---
+++ src/components/2__MainRightCTN.d.ts
@@ -0,0 +1,2 @@
+declare const MainRightCTN: () => import("react/jsx-runtime").JSX.Element;
+export default MainRightCTN;
---
+++ src/components/components/2.2__SocmedSectBtn.d.ts
@@ -0,0 +1,2 @@
+declare const SocmedSectBtn: () => import("react/jsx-runtime").JSX.Element;
+export default SocmedSectBtn;
