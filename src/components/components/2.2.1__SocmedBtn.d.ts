b3ffcde33c3fe1b22d03c5f3c72dbcdfe5341baa
Mark Siazon <markangelosiazon@gmail.com>
2025-01-08 22:48:08 +0800

    🔨 Minor Fix | TypeScript error

--- src/components/components/2.2.1__SocmedBtn.d.ts
+++ src/components/components/2.2.1__SocmedBtn.d.ts
@@ -2,6 +2,7 @@ import React from "react";
 interface SocmedBtnProps {
     href: string;
     icon: React.ReactNode;
+    ariaLabel?: string;
 }
 declare const SocmedBtn: React.FC<SocmedBtnProps>;
 export default SocmedBtn;
